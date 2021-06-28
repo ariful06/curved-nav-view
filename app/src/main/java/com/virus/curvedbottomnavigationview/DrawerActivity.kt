6e18db1e38597a9b6f6e0769041b699354277bb2
Arif <arif.cse36@gmail.com>
2021-06-28 17:33:53 +0600

    added click event on items

--- app/src/main/java/com/virus/curvedbottomnavigationview/DrawerActivity.kt
+++ app/src/main/java/com/virus/curvedbottomnavigationview/DrawerActivity.kt
@@ -1,29 +1,92 @@
 package com.virus.curvedbottomnavigationview
 
-
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.virus.curvedbottomnavigationview.drawer.HomeFragment
+import com.virus.curvedbottomnavigationview.drawer.ProfileFragment
+import com.virus.curvedbottomnavigationview.drawer.SettingsFragment
+import com.virus.curvedbottomnavigationview.drawer.ShareFragment
 import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout
 import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle
 
 
 class DrawerActivity : AppCompatActivity() {
+
+    private var drawerLayout: DuoDrawerLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawer)
+        init()
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        val drawerLayout = findViewById<View>(R.id.drawer) as DuoDrawerLayout
+    }
 
+    private fun init() {
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById<View>(R.id.drawer) as DuoDrawerLayout
         val drawerToggle = DuoDrawerToggle(
-            this, drawerLayout,toolbar,
+            this, drawerLayout, toolbar,
             R.string.navigation_drawer_open,
             R.string.navigation_drawer_close
         )
 
-        drawerLayout.setDrawerListener(drawerToggle)
+        drawerLayout?.setDrawerListener(drawerToggle)
         drawerToggle.syncState()
+
+
+        val contentView = drawerLayout?.contentView
+        val menuView = drawerLayout?.menuView
+
+        val ll_Home = menuView?.findViewById<LinearLayout>(R.id.ll_Home)
+        val ll_Profile = menuView?.findViewById<LinearLayout>(R.id.ll_Profile)
+        val ll_Setting = menuView?.findViewById<LinearLayout>(R.id.ll_Setting)
+        val ll_Share = menuView?.findViewById<LinearLayout>(R.id.ll_Share)
+        val ll_Logout = menuView?.findViewById<LinearLayout>(R.id.ll_Logout)
+
+
+        ll_Home?.setOnClickListener {
+            closeDrawer()
+            replace(HomeFragment())
+        }
+        ll_Profile?.setOnClickListener {
+            closeDrawer()
+            replace(ProfileFragment())
+
+        }
+        ll_Setting?.setOnClickListener {
+            closeDrawer()
+            replace(SettingsFragment())
+
+        }
+        ll_Share?.setOnClickListener {
+            closeDrawer()
+            replace(ShareFragment())
+
+        }
+        ll_Logout?.setOnClickListener { Toast.makeText(this, "Logout", Toast.LENGTH_LONG).show() }
+
+        replace(HomeFragment())
+
+    }
+
+    fun closeDrawer() {
+        if (drawerLayout?.isDrawerOpen != null && drawerLayout?.isDrawerOpen!!) {
+            drawerLayout?.closeDrawer(Gravity.START);
+        }
+    }
+
+    private fun replace(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/virus/curvedbottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/virus/curvedbottomnavigationview/MainActivity.kt
@@ -1,27 +1,50 @@
 package com.virus.curvedbottomnavigationview
 
+import android.provider.Settings
+import com.virus.curvedbottomnavigationview.drawer.HomeFragment
+import com.virus.curvedbottomnavigationview.drawer.ProfileFragment
+import com.virus.curvedbottomnavigationview.drawer.SettingsFragment
+import com.virus.curvedbottomnavigationview.drawer.ShareFragment
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.view.menu.MenuAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener
+{
 
-
+    private var bottomNavigationView : BottomNavigationView?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.background = null
+        bottomNavigationView  = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView?.background = null
         val fab = findViewById<FloatingActionButton>(R.id.fab)
+
         fab.setOnClickListener {
             val intent = Intent(this, DrawerActivity::class.java)
             startActivity(intent)
         }
+        bottomNavigationView?.setOnNavigationItemSelectedListener(this)
+        replace(HomeFragment())
     }
-
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home ->replace(HomeFragment())
+            R.id.action_profile ->replace(ProfileFragment())
+            R.id.action_settings ->replace(SettingsFragment())
+            R.id.action_share ->replace(ShareFragment())
+        }
+        return true // not false!
+    }
+    private fun replace(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
 }
